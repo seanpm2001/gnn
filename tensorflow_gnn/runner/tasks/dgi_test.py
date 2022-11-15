5c62e539f2a4130b5b983e3c14fc9217e028c6e7
Neslihan Bulut <neslihanbulut@google.com>
2022-11-15 13:36:15 -0800

    Apply DGI Task random seed for feature shuffling operations.
    PiperOrigin-RevId: 488745861

--- tensorflow_gnn/runner/tasks/dgi_test.py
+++ tensorflow_gnn/runner/tasks/dgi_test.py
@@ -46,19 +46,19 @@ edge_sets {
 
 class DeepGraphInfomaxTest(tf.test.TestCase):
 
+  seed = 8191
   gtspec = tfgnn.create_graph_spec_from_schema_pb(tfgnn.parse_schema(SCHEMA))
-  task = dgi.DeepGraphInfomax("node", seed=8191)
+  task = dgi.DeepGraphInfomax("node", seed=seed)
 
   def build_model(self):
     graph = inputs = tf.keras.layers.Input(type_spec=self.gtspec)
 
     for _ in range(2):  # Message pass twice
       values = tfgnn.broadcast_node_to_edges(
-          graph,
-          "edge",
-          tfgnn.TARGET,
-          feature_name=tfgnn.HIDDEN_STATE)
-      messages = tf.keras.layers.Dense(16)(values)
+          graph, "edge", tfgnn.TARGET, feature_name=tfgnn.HIDDEN_STATE)
+      messages = tf.keras.layers.Dense(
+          16, kernel_initializer=tf.constant_initializer(1.))(
+              values)
 
       pooled = tfgnn.pool_edges_to_node(
           graph,
@@ -69,7 +69,9 @@ class DeepGraphInfomaxTest(tf.test.TestCase):
       h_old = graph.node_sets["node"].features[tfgnn.HIDDEN_STATE]
 
       h_next = tf.keras.layers.Concatenate()((pooled, h_old))
-      h_next = tf.keras.layers.Dense(8)(h_next)
+      h_next = tf.keras.layers.Dense(
+          8, kernel_initializer=tf.constant_initializer(1.))(
+              h_next)
 
       graph = graph.replace_features(
           node_sets={"node": {
@@ -96,6 +98,43 @@ class DeepGraphInfomaxTest(tf.test.TestCase):
     model = self.task.adapt(self.build_model())
     model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True))
     model.fit(ds)
+
+  def test_deterministic_shuffle(self):
+    gt = tfgnn.GraphTensor.from_pieces(
+        node_sets={
+            "node":
+                tfgnn.NodeSet.from_fields(
+                    features={
+                        tfgnn.HIDDEN_STATE:
+                            tf.convert_to_tensor([[1., 2., 3., 4.],
+                                                  [11., 11., 11., 11.],
+                                                  [19., 19., 19., 19.]])
+                    },
+                    sizes=tf.convert_to_tensor([3])),
+        },
+        edge_sets={
+            "edge":
+                tfgnn.EdgeSet.from_fields(
+                    sizes=tf.convert_to_tensor([2]),
+                    adjacency=tfgnn.Adjacency.from_indices(
+                        ("node", tf.convert_to_tensor([0, 1], dtype=tf.int32)),
+                        ("node", tf.convert_to_tensor([0, 0], dtype=tf.int32)),
+                    )),
+        })
+    xs = tf.data.Dataset.from_tensors(gt).repeat(8)
+    ys = tf.data.Dataset.from_tensors([1., 0.]).repeat()
+    ds = tf.data.Dataset.zip((xs, ys))
+    ds = ds.batch(4).map(lambda x, y: (x.merge_batch_to_components(), y))
+
+    tf.random.set_seed(self.seed)
+    model = self.task.adapt(self.build_model())
+    model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True))
+    model.fit(ds)
+    losses = []
+    for _ in range(2):
+      tf.random.set_seed(self.seed)
+      losses.append(model.evaluate(ds))
+    self.assertAllClose(losses[0], losses[1], rtol=1e-5, atol=1e-5)
 
   def test_embeddings_submodule(self):
     model = self.task.adapt(self.build_model())
